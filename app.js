a5ff12e4c9cbc4c2dd5f0a55b253b4c2ae1f45db
alletsz <stellazg@gmail.com>
2019-01-16 15:43:32 -0800

    Implement GroceryList component with properties

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+var GroceryList = (props) => {
+
+  var onGroceryListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onGroceryListItemClick}>{props.groceryItem[0]}</li>
+      <li onClick={onGroceryListItemClick}>{props.groceryItem[1]}</li>
+    </ul>
+  );
+};
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceryItem={['Cucumbers', 'Kale']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
